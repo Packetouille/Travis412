42962a9cecf4df84cd5989678f5405db7a12b431
Andres Londono <alondo1@students.towson.edu>
2026-03-02 20:34:31 -0500

    Added test to subtract, multiply, and divide.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -16,17 +16,25 @@ public class SimpleCalculatorTest {
 	public void testSubtract() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.subtract(5, 3), 2);
+		assertEquals(calc.subtract(10, 5), 5);
+		assertEquals(calc.subtract(100, 100), 0);
+		assertEquals(calc.subtract(100, 200), -100);
 	}
 
 	@Test
 	public void testMultiply() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.multiply(2, 4), 8);
+		assertEquals(calc.multiply(-10, 10), -100);
+		assertEquals(calc.multiply(-5, -5), 25);
+		assertEquals(calc.multiply(0, 10), 0);
 	}
 
 	@Test
 	public void testDivide() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.divide(8, 2), 4);
+		assertEquals(calc.divide(100, 100), 1);
+		assertEquals(calc.divide(-100, 10), -10);
 	}
 }
